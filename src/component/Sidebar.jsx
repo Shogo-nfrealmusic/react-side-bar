b63003f4ce8950fb73f62bfe73f498abc9f09721
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-04-16 21:02:28 +0900

    sidebar css and active

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -7,11 +7,15 @@ const Sidebar = () => {
         <ul className='SidebarList'>
           {SidebarData.map((value, key) => {
             return (
-              <li key={key} className="row" onClick={() => {
+              <li 
+              key={key} 
+              id={window.location.pathname == value.link ? "active" : ""}
+              className="row" 
+              onClick={() => {
                 window.location.pathname = value.link;
               }}>
                 <div id='icon'>{value.icon}</div>
-                <div id='icon'>{value.title}</div>
+                <div id='title'>{value.title}</div>
               </li>
             )
           })}
