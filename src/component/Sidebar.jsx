844335f547f1dff114820568bb42c6fd9b55a840
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-04-16 20:52:35 +0900

    npm icon

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { SidebarData } from './SidebarData';
 
 const Sidebar = () => {
   return (
     <div className='Sidebar'>
         <ul className='SidebarList'>
-        
+          {SidebarData.map((value, key) => {
+            return (
+              <li key={key} className="row" onClick={() => {
+                window.location.pathname = value.link;
+              }}>
+                <div id='icon'>{value.icon}</div>
+                <div id='icon'>{value.title}</div>
+              </li>
+            )
+          })}
         </ul>
     </div>
   )
--- src/component/SidebarData.jsx
+++ src/component/SidebarData.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
+
+import React from "react";
+import HomeIcon from '@material-ui/icons/Home';
+import MailIcon from '@material-ui/icons/Mail';
+import AssessmentIcon from '@material-ui/icons/Assessment';
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
+import PaymentIcon from '@material-ui/icons/Payment';
+import BackupIcon from '@material-ui/icons/Backup';
+import SettingsIcon from '@material-ui/icons/Settings';
 
 export const SidebarData = [
-    {
-        title: "HOME",
-        icon:,
-        link: "/home"
-    }
-]
-  
-
-
+  {
+    title: "ホーム",
+    icon: <HomeIcon />,
+    link: "/home",
+  },
+  {
+    title: "メール",
+    icon: <MailIcon />,
+    link: "/mail",
+  },
+  {
+    title: "アナリティクス",
+    icon: <AssessmentIcon />,
+    link: "/analitics",
+  },
+  {
+    title: "友達追加",
+    icon: <PersonAddIcon />,
+    link: "/friends",
+  },
+  {
+    title: "お支払い設定",
+    icon: <PaymentIcon />,
+    link: "/payment",
+  },
+  {
+    title: "アップロード",
+    icon: <BackupIcon />,
+    link: "/upload",
+  },
+  {
+    title: "詳細設定",
+    icon: <SettingsIcon />,
+    link: "/settings",
+  },
+];
